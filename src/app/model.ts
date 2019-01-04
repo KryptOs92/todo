ff07ca7ab536e783962b1edb126d5903e9591b9e
Michele <micheleroma92@gmail.com>
2019-01-04 13:13:31 +0100

    Aggiunto model typescript

---
+++ src/app/model.ts
@@ -0,0 +1,7 @@
+const model = {
+  user: 'adam',
+  items: [{action: 'Buy flowers', done: false},
+          {action: 'Get shoes', done: false},
+          {action: 'Collect Tickets', done: true},
+          {action: 'Call Joe', done: false}]
+};
